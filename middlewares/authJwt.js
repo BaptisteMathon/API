784c254ab4059c0f1bdda3f94394e8e5feb5fb9f
BaptisteMathon <103061783+BaptisteMathon@users.noreply.github.com>
2024-11-21 11:57:10 +0100

    Initial Commit

---
+++ middlewares/authJwt.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/key.js");
+const User = require("../models/user.js");
+
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  } 
+  console.log(token);
+  console.log(config.secret);
+
+  if(token === config.secret){
+    console.log("token ok")
+  }
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+        console.log(err)
+        return res.status(401).send({message: "Unauthorized!"});
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+isExist = async (req, res, next) => {
+  const user = await User.findById(req.userId);
+  if (!user) {
+    res.status(403).send({ message: "User not found" });
+    return;
+  
+  }
+  console.log(user.username)
+  console.log(user.admin)
+  if (!user.admin) {
+    res.status(403).send({ message: "User is not admin" });
+    return;
+  }
+  next();
+};
+
+const authJwt = {
+  verifyToken,
+  isExist,
+};
+module.exports = authJwt;
