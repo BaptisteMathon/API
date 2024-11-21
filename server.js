ee298871d0155ed5e0281628f8a19932d4ddd9ea
BaptisteMathon <103061783+BaptisteMathon@users.noreply.github.com>
2024-11-21 20:48:06 +0100

    test

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http');
 const app = require('./app');
 app.set('port',process.env.PORT || 3000);
@@ -8,15 +7,4 @@ server.on('listening', () => {
     const address = server.address();
     //const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
     console.log('Listening on 3000');
-=======
-const http = require('http');
-const app = require('./app');
-app.set('port',process.env.PORT || 3000);
-const server = http.createServer(app);
-server.listen(process.env.PORT || 3000);
-server.on('listening', () => {
-    const address = server.address();
-    //const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on 3000');
->>>>>>> origin/main
 });
